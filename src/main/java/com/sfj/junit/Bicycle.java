bf6e8fee6967e029ed8e8294f12bdd505f73f9eb
Adam Letenyei <letenyeidam89@gmail.com>
2019-11-26 21:51:48 +0100

    Second commit

---
+++ src/main/java/com/sfj/junit/Bicycle.java
@@ -0,0 +1,7 @@
+package com.sfj.junit;
+
+public class Bicycle {
+	public int sum(int a,int b,int c){
+		return a + b + c;
+	}
+}
---
+++ src/test/java/com/sfj/junit/TestBicycle.java
@@ -0,0 +1,5 @@
+package com.sfj.junit;
+
+public class TestBicycle {
+
+}
